08d027dc5c4d7c92f67ebf3664aee667378c7a38
Mauricio Massaia <mauriciomassaia@gmail.com>
2015-06-19 12:45:18 +1000

    added transparent and wireframe options

--- main.js
+++ main.js
@@ -9,14 +9,16 @@
   var scene;
   var stats;
   var uniforms;
-  var worldDepth = 128;
-  var worldWidth = 128;
+  var segments = 64;
   var planeWidth = 2048.0;
   var planeHeight = 2048.0;
+  var shaderMaterial;
 
   var WebGLOptions = function () {
 
     this.radius = 400.0;
+    this.wireframe = true;
+    this.transparent = true;
 
   };
 
@@ -26,21 +28,33 @@
   function initDatGui() {
 
     var gui = new dat.GUI();
-    var controller = gui.add(opts, 'radius', 10.0, 1000.0, 10.0 );
-    controller.onChange( function ( value ) {
-       uniforms.radius.value = value;
-    });
+    gui.add(opts, 'radius', 10.0, 1000.0, 10.0 )
+      .onChange( function ( value ) {
+         uniforms.radius.value = value;
+      });
+
+    gui.add(opts, 'wireframe')
+      .onChange(function (value) {
+        shaderMaterial.wireframe = value;
+      });
+
+    gui.add(opts, 'transparent')
+      .onChange(function (value) {
+        shaderMaterial.transparent = value;
+      });
 
   }
 
 
   function initStats() {
+
     stats = new Stats();
     stats.setMode( 0 ); // 0: fps, 1: ms, 2: mb
     stats.domElement.style.position = 'absolute';
     stats.domElement.style.left = '0px';
     stats.domElement.style.top = '0px';
     document.body.appendChild(stats.domElement);
+
   }
 
 
@@ -54,10 +68,10 @@
     camera.position.z = 2500;
     camera.rotation.x = -Math.PI / 4;
 
-    var geometry = new THREE.PlaneBufferGeometry( planeWidth, planeHeight, worldWidth, worldDepth);
+    var geometry = new THREE.PlaneBufferGeometry( planeWidth, planeHeight, segments, segments);
     geometry.applyMatrix( new THREE.Matrix4().makeRotationX( - Math.PI / 2 ) );
 
-    var colors = new THREE.BufferAttribute(new Float32Array( worldWidth * 3 * 4 ), 4 );
+    var colors = new THREE.BufferAttribute(new Float32Array( segments * 3 * 4 ), 4 );
 
     for ( var i = 0; i < colors.length; i ++ ) {
       colors.setXYZW( i, Math.random(), Math.random(), Math.random(), Math.random() );
@@ -71,9 +85,10 @@
       radius: { type: 'f', value: 400.0 }
     };
 
-
-    var shaderMaterial =
+    shaderMaterial =
       new THREE.ShaderMaterial({
+        wireframe: opts.wireframe,
+        transparent: opts.transparent,
         uniforms: uniforms,
         vertexShader:   document.getElementById( 'vertexShader' ).textContent,
         fragmentShader: document.getElementById( 'fragmentShader' ).textContent
@@ -94,31 +109,43 @@
     onWindowResize();
   }
 
+
   // Event Handlers
   function onWindowResize() {
+
     renderer.setSize( window.innerWidth, window.innerHeight );
     camera.aspect = window.innerWidth / window.innerHeight;
     camera.updateProjectionMatrix();
+
   }
 
+
   function animate() {
+
     stats.begin();
     requestAnimationFrame( animate );
     render();
     stats.end();
+
   }
 
+
   function render() {
+
     uniforms.time.value += 0.05;
     camera.updateMatrixWorld();
     renderer.render(scene, camera);
+
   }
 
+
   window.onload = function () {
+
     init();
     initDatGui();
     initStats();
     animate();
+
   };
 
 }());
